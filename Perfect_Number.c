6a4067276b9c1f7eab8fad48bf1cdb6ce051f9b6
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2022-12-31 11:36:30 +0530

    #include<stdio.h> int main () {     int a;     scanf("%d",&a);     int c=0;     for (int i=1;i<a;i++)     {         if (a%i==0)         {             c=c+i;         }     }     if (c==a) printf("True");     else printf("False"); }

---
+++ Perfect_Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    int c=0;
+    for (int i=1;i<a;i++)
+    {
+        if (a%i==0)
+        {
+            c=c+i;
+        }
+    }
+    if (c==a) printf("True");
+    else printf("False");
+}
