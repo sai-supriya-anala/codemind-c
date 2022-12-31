82934dd33f969aeb9510991ae0db1b68a5578ee4
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2022-12-31 12:23:04 +0530

    #include <stdio.h> int main() {     int m,n;     scanf("%d%d",&n,&m);     if(n%2==0 || m%2==0)     {         printf("Player 1");     }     else     {         printf("Player 2");     }
    }

---
+++ Predict_the_winner_in_Coin_Game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d%d",&n,&m);
+    if(n%2==0 || m%2==0)
+    {
+        printf("Player 1");
+    }
+    else
+    {
+        printf("Player 2");
+    }
+    
+}
