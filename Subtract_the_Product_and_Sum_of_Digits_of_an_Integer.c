a0fb95c2d58ab7ff5439b3b5d70d093cffd586ad
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2023-01-06 09:55:22 +0530

    #include<stdio.h> int main () {     int a;     scanf("%d",&a);     int s=0,r,p=1;     while(a!=0)     {         r=a%10;         s=s+r;         p=p*r;         a=a/10;     }     printf("%d",(p-s)); }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    int s=0,r,p=1;
+    while(a!=0)
+    {
+        r=a%10;
+        s=s+r;
+        p=p*r;
+        a=a/10;
+    }
+    printf("%d",(p-s));
+}
